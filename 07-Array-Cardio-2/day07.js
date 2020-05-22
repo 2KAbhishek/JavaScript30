16d608e81cb301e293d8d2ac992f27a3c7eea21a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-22 08:45:55 +0530

    Fix formatting.

--- 07-Array-Cardio-2/day07.js
+++ 07-Array-Cardio-2/day07.js
@@ -1,37 +1,37 @@
 // ## Array Cardio Day 2
 const people = [
-  { name: "Wes", year: 1988 },
-  { name: "Kait", year: 1986 },
-  { name: "Irv", year: 1970 },
-  { name: "Lux", year: 2015 },
+    { name: "Wes", year: 1988 },
+    { name: "Kait", year: 1986 },
+    { name: "Irv", year: 1970 },
+    { name: "Lux", year: 2015 },
 ];
 
 const comments = [
-  { text: "Love this!", id: 523423 },
-  { text: "Super good", id: 823423 },
-  { text: "You are the best", id: 2039842 },
-  { text: "Ramen is my fav food ever", id: 123523 },
-  { text: "Nice Nice Nice!", id: 542328 },
+    { text: "Love this!", id: 523423 },
+    { text: "Super good", id: 823423 },
+    { text: "You are the best", id: 2039842 },
+    { text: "Ramen is my fav food ever", id: 123523 },
+    { text: "Nice Nice Nice!", id: 542328 },
 ];
 
 console.table(people);
 
 // Some and Every Checks
 const anyAdult = people.some(
-  (person) => new Date().getFullYear() - person.year >= 19
+    (person) => new Date().getFullYear() - person.year >= 19
 );
 console.log({ anyAdult });
 
 // Array.prototype.every() // is everyone 19 or older?
 const allAdults = people.every(
-  (person) => new Date().getFullYear() - person.year >= 19
+    (person) => new Date().getFullYear() - person.year >= 19
 );
 console.log({ allAdults });
 
 const childIndex = people.findIndex(
-  (person) => new Date().getFullYear() - person.year <= 19
+    (person) => new Date().getFullYear() - person.year <= 19
 );
-people.splice(childIndex,1);
+people.splice(childIndex, 1);
 
 console.table(people);
 
