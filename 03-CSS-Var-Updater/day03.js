9f098df174061e75edc203f0e810a65ad0ae9f65
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-22 08:52:34 +0530

    Fix formatting for day 3.

--- 03-CSS-Var-Updater/day03.js
+++ 03-CSS-Var-Updater/day03.js
@@ -1,9 +1,12 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll(".controls input");
 
 function updateValue() {
-    const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(
+        `--${this.name}`,
+        this.value + suffix
+    );
 }
 
-inputs.forEach(input => input.addEventListener('change', updateValue));
-inputs.forEach(input => input.addEventListener('mousemove', updateValue));
+inputs.forEach((input) => input.addEventListener("change", updateValue));
+inputs.forEach((input) => input.addEventListener("mousemove", updateValue));
